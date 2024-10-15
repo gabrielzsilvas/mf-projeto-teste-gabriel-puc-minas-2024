aa786e6716570559e563272741e6b674ac7b5be3
Gabriel Silva <gabrielzsilvas@gmail.com>
2024-10-14 22:23:22 -0300

    Função adicionar criada

---
+++ mf-projeto-teste-gabriel-2024/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_projeto_teste_gabriel_2024.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        [Display(Name = "Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
--- mf-projeto-teste-gabriel-2024/Program.cs
+++ mf-projeto-teste-gabriel-2024/Program.cs
@@ -1,3 +1,6 @@
+using mf_projeto_teste_gabriel_2024.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_projeto_teste_gabriel_2024
 {
     public class Program
@@ -10,6 +13,9 @@ namespace mf_projeto_teste_gabriel_2024
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
